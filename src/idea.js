8d9996cb916e4aec80678e1d3e74fee394ad1367
Alex Sodemann <asodemann18@gmail.com>
2020-04-11 13:50:01 -0600

    Add Idea class, create addNewIdea function

---
+++ src/idea.js
@@ -0,0 +1,21 @@
+class Idea {
+  constructor(title, body) {
+    this.id = Date.now();
+    this.title = title;
+    this.body = body;
+    this.star = false;
+  }
+  // saveToStorage() {
+  //
+  // }
+  //
+  // deleteFromStorage() {
+  //
+  // }
+  //
+  // updateIdea() {
+  //
+  // }
+}
+
+//module.exports = Idea;
